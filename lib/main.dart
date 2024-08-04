aae8e0d37affa740511aa6f1b038c0a8fc61f3c1
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-08-05 00:35:27 +1000

    added SimpleFrameApp/BrilliantBluetooth implementation of Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,110 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'simple_frame_app.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MainApp());
 
-class MainApp extends StatelessWidget {
+final _log = Logger("MainApp");
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  MainAppState createState() => MainAppState();
+}
+
+/// SimpleFrameAppState mixin helps to manage the lifecycle of the Frame connection outside of this file
+class MainAppState extends State<MainApp> with SimpleFrameAppState {
+
+  String _message = 'Connect to Frame, then Click "Say Hello!"';
+
+  MainAppState() {
+    Logger.root.level = Level.ALL;
+    Logger.root.onRecord.listen((record) {
+      debugPrint('${record.level.name}: ${record.time}: ${record.message}');
+    });
+  }
+
+  /// subscribe to a ticker feed for the user's selected ticker using their API token
+  @override
+  Future<void> runApplication() async {
+    currentState = ApplicationState.running;
+    if (mounted) setState(() {});
+
+    try {
+      await connectedDevice!.sendString('frame.display.text("Hello, World!", 50, 100)', awaitResponse: false);
+      await Future.delayed(const Duration(milliseconds: 150));
+      await connectedDevice!.sendString('frame.display.show()', awaitResponse: false);
+      _message = 'Hello World sent!';
+    } catch (e) {
+      _log.fine('Error executing application logic: $e');
+    }
+
+    currentState = ApplicationState.ready;
+    if (mounted) setState(() {});
+  }
+
+  @override
+  Future<void> stopApplication() async {
+    currentState = ApplicationState.stopping;
+    if (mounted) setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    // work out the states of the footer buttons based on the app state
+    List<Widget> pfb = [];
+
+    switch (currentState) {
+      case ApplicationState.disconnected:
+        pfb.add(TextButton(onPressed: scanOrReconnectFrame, child: const Text('Connect Frame')));
+        pfb.add(const TextButton(onPressed: null, child: Text('Say Hello')));
+        pfb.add(const TextButton(onPressed: null, child: Text('Finish')));
+        break;
+
+      case ApplicationState.scanning:
+      case ApplicationState.connecting:
+      case ApplicationState.stopping:
+      case ApplicationState.disconnecting:
+        pfb.add(const TextButton(onPressed: null, child: Text('Connect Frame')));
+        pfb.add(const TextButton(onPressed: null, child: Text('Say Hello')));
+        pfb.add(const TextButton(onPressed: null, child: Text('Finish')));
+        break;
+
+      case ApplicationState.ready:
+        pfb.add(const TextButton(onPressed: null, child: Text('Connect Frame')));
+        pfb.add(TextButton(onPressed: runApplication, child: const Text('Say Hello')));
+        pfb.add(TextButton(onPressed: disconnectFrame, child: const Text('Finish')));
+        break;
+
+      case ApplicationState.running:
+        pfb.add(const TextButton(onPressed: null, child: Text('Connect Frame')));
+        pfb.add(TextButton(onPressed: stopApplication, child: const Text('Say Hello')));
+        pfb.add(const TextButton(onPressed: null, child: Text('Finish')));
+        break;
+    }
+
+    return MaterialApp(
+      title: 'Hello World!',
+      theme: ThemeData.dark(),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text("Hello World!"),
         ),
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 150),
+                Text(_message),
+              ]
+            ),
+          )
+        ),
+        persistentFooterButtons: pfb,
       ),
     );
   }
