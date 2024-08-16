f2b38edb3cf6659e606b23b818dd0671dbe01d3d
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-08-16 12:01:46 +1000

    updated simple_frame_app

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,14 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
   String _message = 'Connect to Frame, Start the Application, then Click "Say Hello!"';
 
   MainAppState() {
-    Logger.root.level = Level.ALL;
+    Logger.root.level = Level.INFO;
     Logger.root.onRecord.listen((record) {
       debugPrint('${record.level.name}: ${record.time}: ${record.message}');
     });
   }
 
   /// send a simple message to the frame display
+  @override
   Future<void> run() async {
     currentState = ApplicationState.running;
     if (mounted) setState(() {});
@@ -37,14 +38,16 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
       await Future.delayed(const Duration(milliseconds: 150));
       await frame!.sendString('frame.display.show()', awaitResponse: false);
       _message = 'Hello World sent!';
+
     } catch (e) {
-      _log.fine('Error executing application logic: $e');
+      _log.severe('Error executing application logic: $e');
     }
 
     currentState = ApplicationState.ready;
     if (mounted) setState(() {});
   }
 
+  @override
   Future<void> cancel() async {
     currentState = ApplicationState.ready;
     if (mounted) setState(() {});
@@ -52,45 +55,6 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
 
   @override
   Widget build(BuildContext context) {
-    // work out the states of the footer buttons based on the app state
-    List<Widget> pfb = [];
-
-    switch (currentState) {
-      case ApplicationState.disconnected:
-        pfb.add(TextButton(onPressed: scanOrReconnectFrame, child: const Text('Connect')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Start')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Disconnect')));
-        break;
-
-      case ApplicationState.initializing:
-      case ApplicationState.scanning:
-      case ApplicationState.connecting:
-      case ApplicationState.stopping:
-      case ApplicationState.disconnecting:
-        pfb.add(const TextButton(onPressed: null, child: Text('Connect')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Start')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Disconnect')));
-        break;
-
-      case ApplicationState.connected:
-        pfb.add(const TextButton(onPressed: null, child: Text('Connect')));
-        pfb.add(TextButton(onPressed: startApplication, child: const Text('Start')));
-        pfb.add(TextButton(onPressed: disconnectFrame, child: const Text('Disconnect')));
-        break;
-
-      case ApplicationState.ready:
-        pfb.add(const TextButton(onPressed: null, child: Text('Connect')));
-        pfb.add(TextButton(onPressed: stopApplication, child: const Text('Stop')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Disconnect')));
-        break;
-
-      case ApplicationState.running:
-        pfb.add(const TextButton(onPressed: null, child: Text('Connect')));
-        pfb.add(TextButton(onPressed: stopApplication, child: const Text('Stop')));
-        pfb.add(const TextButton(onPressed: null, child: Text('Disconnect')));
-        break;
-    }
-
     return MaterialApp(
       title: 'Hello World!',
       theme: ThemeData.dark(),
@@ -109,10 +73,8 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
             ),
           )
         ),
-        floatingActionButton: currentState == ApplicationState.ready ?
-          FloatingActionButton(onPressed: run, child: const Icon(Icons.message_outlined)) :
-          FloatingActionButton(onPressed: cancel, child: const Icon(Icons.cancel)),
-        persistentFooterButtons: pfb,
+        floatingActionButton: getFloatingActionButtonWidget(const Icon(Icons.message_outlined), const Icon(Icons.cancel)),
+        persistentFooterButtons: getFooterButtonsWidget(),
       ),
     );
   }
